76be62dc6905e7bccb37be8ff3cfd75ba2da0a2d
Oumar <oumarmauret@gmail.com>
2022-04-08 12:22:57 +0000

    Add user login and login routes

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,6 +1,16 @@
 const models = require('../models');
 const User = models.User;
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+
+const maxAge = 3 * 24 * 60 * 60 * 1000;
+const createToken = (id) => {
+
+    return jwt.sign({ id }, process.env.TOKEN_SECRET, {
+
+        expiresIn: maxAge// token validate for  3 days
+    })
+};
 
 module.exports.signUp = (req, res) => {
     //Destructuring data is equivalent = firstname = req.body.firstname
@@ -25,5 +35,40 @@ module.exports.signUp = (req, res) => {
     } catch (err) {
         res.status(200).send({ err: err })
     }
+
+}
+//Login User
+module.exports.signIn = async (req, res) => {
+    const { email, password } = req.body;
+
+    try {
+        const user = await User.findOne({ where: { email } })
+        if (user) {
+
+            //Compare User password input with database Password
+            const auth = bcrypt.compare(password, user.password);
+            if (auth) {
+                const token = createToken(user.id);
+                res.cookie('jwt', token, { httpOnly: true, maxAge })
+                res.status(200).json({ "user": user.id })
+            } else {
+                return res.status(400).json({ 'message': 'Incorrect password' })
+            }
+
+        }
+
+    } catch (err) {
+        return res.status(404).json(err)
+    }
+}
+
+//Disconnected User
+
+module.exports.logout = async (req, res) => {
+    //Delete cookie values
+    res.cookie('jwt', '', { maxAge: 1 });
+
+    //Redirect with login page
+    res.redirect('/');
 
 }
--- backend/routes/user.routes.js
+++ backend/routes/user.routes.js
@@ -2,7 +2,12 @@ const router = require('express').Router();
 const authController = require('../controllers/auth.controller');
 const userController = require('../controllers/user.controller');
 
+//Authentification 
 router.post('/register', authController.signUp);
+router.post('/login', authController.signIn);
+router.get('/logout', authController.logout);
+
+//user profil 
 router.get('/', userController.getAllUsers);
 router.get('/:id', userController.userInfo);
 router.put('/:id', userController.UpdateUser);
